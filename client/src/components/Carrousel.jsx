adf35dad477da9d49c6be522d24e1ab462b41ac4
oversightdolores <oversightdolores@gmail.com>
2023-03-27 14:13:51 -0300

    carrousel

---
+++ client/src/components/Carrousel.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Carrousel = () => {
+  return (
+    <View>
+      <Text>Carrousel</Text>
+    </View>
+  );
+};
+
+export default Carrousel;
