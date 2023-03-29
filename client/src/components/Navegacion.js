fe093b07f533a4818dbd76906803858971097dc0
Sol <solabigail00@hotmail.com>
2023-03-29 14:33:49 -0300

    Add Component PostDetail

--- client/src/components/Navegacion.js
+++ client/src/components/Navegacion.js
@@ -2,13 +2,14 @@ import { View, Text } from "react-native";
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../pages/Home";
+import PostDetail from "./PostDetail";
 
 const Stack = createStackNavigator();
 
 const Navegacion = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Home" component={PostDetail} />
     </Stack.Navigator>
   );
 };
