9084cf69ec19b918f8a736f40f1812dd432af194
Erick <erfacrypto@gmail.com>
2023-03-31 14:23:27 -0300

    headerShown:false

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,25 +3,27 @@ import React, { useState, useEffect } from "react";
 import Cabecera from "../components/Cabecera";
 import Carrousel from "../components/Carrousel";
 import { ScrollView } from "react-native-gesture-handler";
+import Login from "../components/Login";
 
 
 const Home = () => {
   return (
-    <View className="flex-1 " style={{ backgroundColor: "#EAE0F4" }}>
-      <Cabecera />
-      <ScrollView className="flex-1 my-3 ">
-        <View className="flex-1 ">
-          <View>
-            <Text className=" font-black "> Productos </Text>
-            <Carrousel />
-          </View>
-          <View>
-            <Text className=" font-black "> Servicios </Text>
-            <Carrousel />
-          </View>
-        </View>
-      </ScrollView>
-    </View>
+    <Login/>
+    // <View className="flex-1 " style={{ backgroundColor: "#EAE0F4" }}>
+    //   <Cabecera />
+    //   <ScrollView className="flex-1 my-3 ">
+    //     <View className="flex-1 ">
+    //       <View>
+    //         <Text className=" font-black "> Productos </Text>
+    //         <Carrousel />
+    //       </View>
+    //       <View>
+    //         <Text className=" font-black "> Servicios </Text>
+    //         <Carrousel />
+    //       </View>
+    //     </View>
+    //   </ScrollView>
+    // </View>
   );
 };
 
